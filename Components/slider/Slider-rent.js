e56910f08233fc9b9e2723a0d3b9a6e99de9bf9d
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-09-02 12:39:43 +0300

    add: slider to rent page

--- Components/slider/Slider-rent.js
+++ Components/slider/Slider-rent.js
@@ -5,15 +5,35 @@ import Fancybox from "./Fancybox";
 
 import styles from "../../styles/slider.module.scss";
 import Image from "next/image";
-import image1 from "../../public/slider/land/image1.JPG";
-import image2 from "../../public/slider/land/image2.JPG";
-import image3 from "../../public/slider/land/image3.JPG";
+import image1 from "../../public/slider/rent/1.JPG";
+import image2 from "../../public/slider/rent/2.JPG";
+import image3 from "../../public/slider/rent/3.JPG";
+import image4 from "../../public/slider/rent/4.JPG";
+import image5 from "../../public/slider/rent/5.JPG";
+import image6 from "../../public/slider/rent/6.JPG";
+import image7b from "../../public/slider/rent/7b.JPG";
+import image7c from "../../public/slider/rent/7c.JPG";
+import image7d from "../../public/slider/rent/7d.JPG";
+import image8 from "../../public/slider/rent/8.jpg";
+import image9 from "../../public/slider/rent/9.jpg";
+
+import image10 from "../../public/slider/rent/10.JPG";
+
+import image11 from "../../public/slider/rent/11.JPG";
+import image12 from "../../public/slider/rent/12.JPG";
+import image13 from "../../public/slider/rent/13.JPG";
+import image14 from "../../public/slider/rent/14.JPG";
+import image15 from "../../public/slider/rent/15.JPG";
+import image16 from "../../public/slider/rent/16.JPG";
+import image17 from "../../public/slider/rent/17.JPG";
+import image18 from "../../public/slider/rent/18.JPG";
+import image19 from "../../public/slider/rent/20a.JPG";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/scrollbar";
 
-export default function SliderRent() {
+export default function Sliderrent() {
   return (
     <>
       <div className={styles.container_slider}>
@@ -55,7 +75,7 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image1_hetxjw.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110044/rent/slider-auto/1_daksn4.jpg"
               >
                 <Image
                   className={styles.imageSlide}
@@ -69,7 +89,7 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886594/rent/slider-land/image2_i6yv3a.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110044/rent/slider-auto/2_a9hhxw.jpg"
               >
                 <Image
                   className={styles.imageSlide}
@@ -83,7 +103,7 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image3_nsdyrj.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110041/rent/slider-auto/3_uduo1e.jpg"
               >
                 <Image
                   className={styles.imageSlide}
@@ -97,11 +117,11 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image1_hetxjw.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110041/rent/slider-auto/4_rt5lfg.jpg"
               >
                 <Image
                   className={styles.imageSlide}
-                  src={image1}
+                  src={image4}
                   alt="land"
                   width={610}
                   height={397}
@@ -111,11 +131,11 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image1_hetxjw.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110041/rent/slider-auto/5_a1wek7.jpg"
               >
                 <Image
                   className={styles.imageSlide}
-                  src={image1}
+                  src={image5}
                   alt="land"
                   width={610}
                   height={397}
@@ -125,11 +145,11 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image1_hetxjw.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110041/rent/slider-auto/6_dutj8g.jpg"
               >
                 <Image
                   className={styles.imageSlide}
-                  src={image1}
+                  src={image6}
                   alt="land"
                   width={610}
                   height={397}
@@ -139,11 +159,11 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image1_hetxjw.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110041/rent/slider-auto/7b_us2jpm.jpg"
               >
                 <Image
                   className={styles.imageSlide}
-                  src={image1}
+                  src={image7b}
                   alt="land"
                   width={610}
                   height={397}
@@ -153,11 +173,11 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image1_hetxjw.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110041/rent/slider-auto/7c_ovspqu.jpg"
               >
                 <Image
                   className={styles.imageSlide}
-                  src={image1}
+                  src={image7c}
                   alt="land"
                   width={610}
                   height={397}
@@ -167,11 +187,11 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image1_hetxjw.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110042/rent/slider-auto/7d_stywlh.jpg"
               >
                 <Image
                   className={styles.imageSlide}
-                  src={image1}
+                  src={image7d}
                   alt="land"
                   width={610}
                   height={397}
@@ -181,11 +201,165 @@ export default function SliderRent() {
             <SwiperSlide>
               <a
                 data-fancybox="gallery"
-                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1661886592/rent/slider-land/image1_hetxjw.jpg"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110042/rent/slider-auto/8_jfiy78.jpg"
               >
                 <Image
                   className={styles.imageSlide}
-                  src={image1}
+                  src={image8}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110042/rent/slider-auto/9_qnostw.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image9}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110042/rent/slider-auto/10_kjtxma.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image10}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110042/rent/slider-auto/11_if8kkf.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image11}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110042/rent/slider-auto/12_nozfdl.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image12}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110043/rent/slider-auto/13_aq0mts.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image13}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110043/rent/slider-auto/14_rt5b6r.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image14}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110043/rent/slider-auto/15_mit9oj.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image15}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110043/rent/slider-auto/16_ysp52q.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image16}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110043/rent/slider-auto/17_xvilvd.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image17}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110043/rent/slider-auto/18_rfyaeq.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image18}
+                  alt="land"
+                  width={610}
+                  height={397}
+                />
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a
+                data-fancybox="gallery"
+                href="https://res.cloudinary.com/dzpwjmq0x/image/upload/v1662110044/rent/slider-auto/20a_kyj36t.jpg"
+              >
+                <Image
+                  className={styles.imageSlide}
+                  src={image19}
                   alt="land"
                   width={610}
                   height={397}
