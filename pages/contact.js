cf891269d841860130598f5f12dd33378188a162
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-09-02 15:55:05 +0300

    add: blocks to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/land.module.scss";
+import styles from "../styles/contact.module.scss";
 import { Menu } from "../Components/Menu";
 import { Footer } from "../Components/Footer";
 import Formcontact from "../Components/Formcontact";
@@ -20,6 +20,18 @@ export default function Contact() {
         <Menu />
       </div>
       <Formcontact />
+      <div className={styles.container_dowland}>
+        <div className={styles.dowland}>
+          <h4>Downloads</h4>
+          <p>Coming soon.</p>
+        </div>
+      </div>
+      <div className={styles.container_payment}>
+        <div className={styles.payment}>
+          <h4>Payment Info</h4>
+          <p>Coming soon.</p>
+        </div>
+      </div>
       <Footer />
     </>
   );
