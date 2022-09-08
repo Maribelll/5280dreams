cda098b6c0493b83bdb1cf2eb5966257953cfe85
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-09-08 13:36:28 +0300

    add: changes

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,9 @@ import * as React from "react";
 //for react-pdf
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import { FormspreeProvider } from "@formspree/react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <FormspreeProvider project="{your-project-id}">
-      <Component {...pageProps} />
-    </FormspreeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
